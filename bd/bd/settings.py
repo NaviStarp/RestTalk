3cadc5c7a56b3347c06cf4deba4b554366455290
Sebastian-Manrique <smanriquemontiel@gmail.com>
2025-02-19 21:35:07 +0100

    Now the users can create an account

--- bd/bd/settings.py
+++ bd/bd/settings.py
@@ -191,6 +191,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Default primary key field type
+# CRAIZI
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
