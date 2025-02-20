b50b33d7834156d5f2232b39b257e9c4de65fbeb
Javier <javier.rod.jim2005@gmail.com>
2025-02-20 20:03:13 +0100

    bsfja

--- chat/src/app/header/header.component.ts
+++ chat/src/app/header/header.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+CommonModule
 @Component({
   selector: 'app-header',
-  imports: [], 
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent  {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu toggled:', this.isMenuOpen);
+  }
+  
 }
--- chat/src/app/privacidad/privacidad.component.ts
+++ chat/src/app/privacidad/privacidad.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { InicioComponent } from '../inicio/inicio.component';
 import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-privacidad',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent,InicioComponent],
   templateUrl: './privacidad.component.html',
   styleUrl: './privacidad.component.css'
 })
