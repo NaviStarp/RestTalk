1b30b51581114e4b36620c40604a169b556c0de2
Javier <javier.rod.jim2005@gmail.com>
2025-02-21 18:39:46 +0100

    login y registro

--- chat/src/app/inicio-sesion/inicio-sesion.component.ts
+++ chat/src/app/inicio-sesion/inicio-sesion.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
--- chat/src/app/registro/registro.component.ts
+++ chat/src/app/registro/registro.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+
 
 @Component({
   selector: 'app-registro',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
