e1abaa5ece9d58c6be75331e5f833db7f2da08bb
Sebastian-Manrique <smanriquemontiel@gmail.com>
2025-02-26 21:18:32 +0100

    Adding the dark mode to the chat web

--- chat/src/app/conversacion-chat/conversacion-chat.component.ts
+++ chat/src/app/conversacion-chat/conversacion-chat.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CambiarColorService } from '../services/cambiar-color.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-conversacion-chat', // Asegúrate de que el selector sea correcto
+  selector: 'app-conversacion-chat',
   templateUrl: './conversacion-chat.component.html',
-  styleUrls: ['./conversacion-chat.component.css']
+  styleUrls: ['./conversacion-chat.component.css'],
+  imports: [CommonModule]
 })
-export class ConversacionChatComponent {
-  // Lógica del componente
+export class ConversacionChatComponent implements OnInit {
+  imagenFondo = 'C:\Users\DAM\Documents\DessarolloDeInterfaces\RestTalk\chat\src\assets\images\fondi.jpg';
+
+  constructor(private cambiarColorService: CambiarColorService) { }
+
+  ngOnInit() {
+    this.cambiarColorService.fondo$.subscribe(estilo => {
+      this.imagenFondo = estilo.imagen;
+    });
+  }
+
+  // Cambiar solo la imagen de fondo
+  cambiarImagen(imagen: string) {
+    this.cambiarColorService.cambiarImagen(imagen);
+  }
 }
